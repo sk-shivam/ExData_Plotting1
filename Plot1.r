7041f9e2cc0899f9e0a124e925abe6dd859cfa83
sk-shivam <68406857+sk-shivam@users.noreply.github.com>
2020-09-30 19:59:04 +0100

    Add files via upload

---
+++ Plot1.r
@@ -0,0 +1,12 @@
+
+data <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", header = T,sep = ";", na.strings = "?")
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
